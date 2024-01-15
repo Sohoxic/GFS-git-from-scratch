9859467562f561af4319d162e232ce82ccb8b260
Sohoxic <pes2202100825@pesu.pes.edu>
2024-01-16 02:19:19 +0530

    added functions repo_file and repo_dir for creating directories

--- libwyag.py
+++ libwyag.py
@@ -49,6 +49,27 @@ class GitRepository (object):
 def repo_path(repo, *path):
     return os.path.join(repo.gitdir, *path)
 
+def repo_file(repo, *path, mkdir=False):
+    """Same as repo_path, but create dirname(*path) if absent.  For example, repo_file(r, \"refs\", \"remotes\", \"origin\", \"HEAD\") will create .git/refs/remotes/origin."""
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+    
+def repo_dir(repo, *path, mkdir=False):
+    """Same as repo_path, but mkdir *path if absent if mkdir."""
+
+    path = repo_path(repo, *path)
+
+    if os.path.exists(path):
+        if (os.path.isdir(path)):
+            return path
+        else:
+            raise Exception("Not a directory %s" % path)
+
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
     match args.command:
