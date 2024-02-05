cd0bea6851a1f1cf04bf8a48546541cb66e0f4bc
Sohoxic <pes2202100825@pesu.pes.edu>
2024-02-05 13:16:19 +0530

    add read object class

--- libwyag.py
+++ libwyag.py
@@ -172,6 +172,40 @@ whatever it takes to convert it into a meaningful representation.  What exactly 
     def init(self):
         pass # Just do nothing. This is a reasonable default!
 
+def object_read(repo, sha):
+    """Read object sha from Git repository repo.  Return a
+    GitObject whose exact type depends on the object."""
+
+    path = repo_file(repo, "objects", sha[0:2], sha[2:])
+
+    if not os.path.isfile(path):
+        return None
+
+    with open (path, "rb") as f:
+        raw = zlib.decompress(f.read())
+
+        # Read object type
+        x = raw.find(b' ')
+        fmt = raw[0:x]
+
+        # Read and validate object size
+        y = raw.find(b'\x00', x)
+        size = int(raw[x:y].decode("ascii"))
+        if size != len(raw)-y-1:
+            raise Exception("Malformed object {0}: bad length".format(sha))
+
+        # Pick constructor
+        match fmt:
+            case b'commit' : c=GitCommit
+            case b'tree'   : c=GitTree
+            case b'tag'    : c=GitTag
+            case b'blob'   : c=GitBlob
+            case _:
+                raise Exception("Unknown type {0} for object {1}".format(fmt.decode("ascii"), sha))
+
+        # Call constructor and return object
+        return c(raw[y+1:])
+
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
     match args.command:
