f7ca651ce6d24947979053e5ebf70ed8712d0c5b
Sohoxic <pes2202100825@pesu.pes.edu>
2024-02-05 12:36:52 +0530

    generic object class

--- libwyag.py
+++ libwyag.py
@@ -151,6 +151,27 @@ def repo_find(path=".", required=True):
     # Recursive case
     return repo_find(parent, required)
 
+class GitObject (object):
+
+    def __init__(self, data=None):
+        if data != None:
+            self.deserialize(data)
+        else:
+            self.init()
+
+    def serialize(self, repo):
+        """This function MUST be implemented by subclasses.
+
+It must read the object's contents from self.data, a byte string, and do
+whatever it takes to convert it into a meaningful representation.  What exactly that means depend on each subclass."""
+        raise Exception("Unimplemented!")
+
+    def deserialize(self, data):
+        raise Exception("Unimplemented!")
+
+    def init(self):
+        pass # Just do nothing. This is a reasonable default!
+
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
     match args.command:
